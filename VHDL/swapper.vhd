7ce427471d86526b03a196211dd9967c9883ccbe
yash <yagasti@gmail.com>
2022-04-16 13:28:11 +1000

    the correct swapper file

--- VHDL/swapper.vhd
+++ VHDL/swapper.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -38,8 +38,30 @@ entity swapper is
 end swapper;
 
 architecture Behavioral of swapper is
-
+    signal b1: unsigned(1 downto 0);
+    signal b2: unsigned(1 downto 0);
+    signal p1: unsigned(2 downto 0);
+    signal p2: unsigned(2 downto 0);
+    signal s: unsigned(2 downto 0);
+    signal data : unsigned(31 downto 0);
+    
 begin
-    data_out <= vote_record;
+    -- TODO: add wraparound for s bit
+    b1 <= unsigned(control_word(1 downto 0));
+    b2 <= unsigned(control_word(3 downto 2));
+    p1 <= unsigned(control_word(6 downto 4));
+    p2 <= unsigned(control_word(9 downto 7));
+    s <= unsigned(control_word(12 downto 10));
+    
+    -- load vote record
+    data <= unsigned(vote_record);
+    
+    -- load part 2 (b2, p2) into position of part 1
+    data((TO_INTEGER(b1) * 8 + TO_INTEGER(p1)) downto (TO_INTEGER(b1) * 8 + TO_INTEGER(p1) - TO_INTEGER(s) + 1)) <= unsigned(vote_record((TO_INTEGER(b2) * 8 + TO_INTEGER(p2)) downto (TO_INTEGER(b2) * 8 + TO_INTEGER(p2) - TO_INTEGER(s) + 1)));
+    -- load part 1 (b1, p1) into position of part 2
+    data((TO_INTEGER(b2) * 8 + TO_INTEGER(p2)) downto (TO_INTEGER(b2) * 8 + TO_INTEGER(p2) - TO_INTEGER(s) + 1)) <= unsigned(vote_record((TO_INTEGER(b1) * 8 + TO_INTEGER(p1)) downto (TO_INTEGER(b1) * 8 + TO_INTEGER(p1) - TO_INTEGER(s) + 1)));
+    
+    -- assign to data out
+    data_out <= std_logic_vector(data);
 
 end Behavioral;
